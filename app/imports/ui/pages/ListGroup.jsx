a6871ff1b2de4e436e3bf5abe68f26d065ed8b63
Arslan Rakhmankulov <31532955+arslan-r@users.noreply.github.com>
2020-08-16 02:40:57 -0700

    added cards

--- app/imports/ui/pages/ListGroup.jsx
+++ app/imports/ui/pages/ListGroup.jsx
@@ -1,42 +1,65 @@
-import React from 'react';
-import { Meteor } from 'meteor/meteor';
-import { Container, Table, Header, Loader } from 'semantic-ui-react';
-import { withTracker } from 'meteor/react-meteor-data';
-import PropTypes from 'prop-types';
-import { Stuffs } from '../../api/stuff/Stuff';
-import { Groups } from '../../api/stuff/Group';
-import StuffItem from '../components/StuffItem';
-import GroupData from '../components/GroupData';
+import React from "react";
+import { Meteor } from "meteor/meteor";
+import { Container, Table, Header, Loader, Card } from "semantic-ui-react";
+import { withTracker } from "meteor/react-meteor-data";
+import PropTypes from "prop-types";
+import { Stuffs } from "../../api/stuff/Stuff";
+import { Groups } from "../../api/stuff/Group";
+import StuffItem from "../components/StuffItem";
+import GroupData from "../components/GroupData";
+import GroupsCard from "../components/GroupsCard";
 
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 class ListGroup extends React.Component {
-
   /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
-    return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
+    return this.props.ready ? (
+      this.renderPage()
+    ) : (
+      <Loader active>Getting data</Loader>
+    );
   }
 
   /** Render the page once subscriptions have been received. */
   renderPage() {
     return (
+      <Container>
+        <Header as="h2" textAlign="center">
+          Your Groups:
+        </Header>
+        <Table celled>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Location</Table.HeaderCell>
+              <Table.HeaderCell>Date</Table.HeaderCell>
+              <Table.HeaderCell>Edit</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {this.props.stuffs.map((stuff) => (
+              <GroupData key={stuff._id} stuff={stuff} />
+            ))}
+          </Table.Body>
+        </Table>
+
         <Container>
-          <Header as="h2" textAlign="center">Your Groups:</Header>
-          <Table celled>
-            <Table.Header>
-              <Table.Row>
-                <Table.HeaderCell>Location</Table.HeaderCell>
-                <Table.HeaderCell>Date</Table.HeaderCell>
-                <Table.HeaderCell>Edit</Table.HeaderCell>
-              </Table.Row>
-            </Table.Header>
-            <Table.Body>
-              {this.props.stuffs.map((stuff) => <GroupData key={stuff._id} stuff={stuff} />)}
-            </Table.Body>
-          </Table>
+          <Card.Group>
+            {this.props.stuffs.map((stuff) => (
+              <GroupsCard key={stuff._id} stuff={stuff} />
+            ))}
+          </Card.Group>
         </Container>
+      </Container>
     );
   }
 }
+/**
+             {_.map(interestData, (interest, index) => ( <MakeCard key={index} interest={interest} />
+            ))}
+
+            {_.map(interestData, (interest, index) => ( <InsertCompHere key={stuff} stuff={stuff} />
+            ))}
+ */
 
 /** Require an array of Stuff documents in the props. */
 ListGroup.propTypes = {
