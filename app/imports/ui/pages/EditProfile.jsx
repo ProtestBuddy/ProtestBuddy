c563e0228d49d008996b06c53f361be60b9a7c8d
Arslan Rakhmankulov <31532955+arslan-r@users.noreply.github.com>
2020-08-16 12:25:55 -0700

    made it!

--- app/imports/ui/pages/EditProfile.jsx
+++ app/imports/ui/pages/EditProfile.jsx
@@ -1,53 +1,75 @@
-import React from 'react';
-import { Grid, Loader, Header, Segment } from 'semantic-ui-react';
-import swal from 'sweetalert';
-import { AutoForm, ErrorsField, HiddenField, NumField, SelectField, SubmitField, TextField } from 'uniforms-semantic';
-import { Meteor } from 'meteor/meteor';
-import { withTracker } from 'meteor/react-meteor-data';
-import PropTypes from 'prop-types';
-import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
-import { Stuffs } from '../../api/stuff/Stuff';
-import { Profile } from '../../api/stuff/Profile';
+import React from "react";
+import { Grid, Loader, Header, Segment } from "semantic-ui-react";
+import swal from "sweetalert";
+import {
+  AutoForm,
+  ErrorsField,
+  HiddenField,
+  NumField,
+  SelectField,
+  SubmitField,
+  TextField,
+} from "uniforms-semantic";
+import { Meteor } from "meteor/meteor";
+import { withTracker } from "meteor/react-meteor-data";
+import PropTypes from "prop-types";
+import SimpleSchema2Bridge from "uniforms-bridge-simple-schema-2";
+import { Stuffs } from "../../api/stuff/Stuff";
+import { Profile } from "../../api/stuff/Profile";
 
 const bridge = new SimpleSchema2Bridge(Profile.schema);
 
 /** Renders the Page for editing a single document. */
 class EditProfile extends React.Component {
-
   /** On successful submit, insert the data. */
   submit(data) {
     const { name, pronouns, gender, age, location, bio, _id } = data;
-    Profile.collection.update(_id, { $set: { name, pronouns, gender, age, location, bio } }, (error) => (error ?
-      swal('Error', error.message, 'error') :
-      swal('Success', 'Item updated successfully', 'success')));
+    Profile.collection.update(
+      _id,
+      { $set: { name, pronouns, gender, age, location, bio } },
+      (error) =>
+        error
+          ? swal("Error", error.message, "error")
+          : swal("Success", "Item updated successfully", "success")
+    );
   }
 
   /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
-    return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
+    return this.props.ready ? (
+      this.renderPage()
+    ) : (
+      <Loader active>Getting data</Loader>
+    );
   }
 
   /** Render the form. Use Uniforms: https://github.com/vazco/uniforms */
   renderPage() {
     return (
-        <Grid container centered>
-          <Grid.Column>
-            <Header as="h2" textAlign="center">Edit Profile:</Header>
-            <AutoForm schema={bridge} onSubmit={data => this.submit(data)} model={this.props.doc}>
-              <Segment>
-                <TextField name='name'/>
-                <TextField name='pronouns'/>
-                <TextField name='gender'/>
-                <NumField name='age' decimal={false}/>
-                <TextField name='location'/>
-                <TextField name='bio'/>
-                <SubmitField value='Submit'/>
-                <ErrorsField/>
-                <HiddenField name='owner' />
-              </Segment>
-            </AutoForm>
-          </Grid.Column>
-        </Grid>
+      <Grid container centered>
+        <Grid.Column>
+          <Header as="h2" textAlign="center">
+            Edit Profile:
+          </Header>
+          <AutoForm
+            schema={bridge}
+            onSubmit={(data) => this.submit(data)}
+            model={this.props.doc}
+          >
+            <Segment>
+              <TextField name="name" />
+              <TextField name="pronouns" />
+              <TextField name="gender" />
+              <NumField name="age" decimal={false} />
+              <TextField name="location" />
+              <TextField name="bio" />
+              <SubmitField value="Submit" />
+              <ErrorsField />
+              <HiddenField name="owner" />
+            </Segment>
+          </AutoForm>
+        </Grid.Column>
+      </Grid>
     );
   }
 }
