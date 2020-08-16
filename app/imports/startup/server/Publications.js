fe114f79b2ce521e8370e8c5902d8b22ddff63ad
Arslan Rakhmankulov <31532955+arslan-r@users.noreply.github.com>
2020-08-16 03:37:01 -0700

    added fetch all stuff

--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -1,9 +1,9 @@
-import { Meteor } from 'meteor/meteor';
-import { Roles } from 'meteor/alanning:roles';
-import { Stuffs } from '../../api/stuff/Stuff';
-import { Profile } from '../../api/stuff/Profile';
+import { Meteor } from "meteor/meteor";
+import { Roles } from "meteor/alanning:roles";
+import { Stuffs } from "../../api/stuff/Stuff";
+import { Profile } from "../../api/stuff/Profile";
 
-import { Groups } from '../../api/stuff/Group';
+import { Groups } from "../../api/stuff/Group";
 
 // User-level publication.
 // If logged in, then publish documents owned by this user. Otherwise publish nothing.
@@ -26,7 +26,7 @@ Meteor.publish(Profile.userPublicationName, function () {
 Meteor.publish(Groups.userPublicationName, function () {
   if (this.userId) {
     const username = Meteor.users.findOne(this.userId).username;
-    return Groups.collection.find({ owner: username });
+    return Groups.collection.find();
   }
   return this.ready();
 });
@@ -34,7 +34,7 @@ Meteor.publish(Groups.userPublicationName, function () {
 // Admin-level publication.
 // If logged in and with admin role, then publish all documents from all users. Otherwise publish nothing.
 Meteor.publish(Stuffs.adminPublicationName, function () {
-  if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
+  if (this.userId && Roles.userIsInRole(this.userId, "admin")) {
     return Stuffs.collection.find();
   }
   return this.ready();
@@ -44,7 +44,7 @@ Meteor.publish(Stuffs.adminPublicationName, function () {
 // Recommended code to publish roles for each user.
 Meteor.publish(null, function () {
   if (this.userId) {
-    return Meteor.roleAssignment.find({ 'user._id': this.userId });
+    return Meteor.roleAssignment.find({ "user._id": this.userId });
   }
   return this.ready();
 });
--- app/imports/ui/pages/ListGroup.jsx
+++ app/imports/ui/pages/ListGroup.jsx
@@ -66,7 +66,7 @@ ListGroup.propTypes = {
   stuffs: PropTypes.array.isRequired,
   ready: PropTypes.bool.isRequired,
 };
-
+//db1: Section1DB.find({}).fetch(),
 /** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
 export default withTracker(() => {
   // Get access to Stuff documents.
